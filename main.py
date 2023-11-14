479a808f8865e76a39af13bd3e480c4931ffcfd6
husseinhareb <shtam.hareb12@gmail.com>
2023-11-14 19:25:37 +0200

    note entry

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import tkinter as tk
-import ttkbootstrap as tb
-from PIL import Image
-from ttkbootstrap.constants import *
-Image.CUBIC = Image.BICUBIC
+from ttkbootstrap import Style, Button, LabelFrame, Window
 
-root = tb.Window(themename="superhero")
+def open_new_window():
+    add_note = tk.Toplevel(root)
+    add_note.title("addnote")
+    add_note.geometry('200x200')
+    note = tk.StringVar()
+
+    # Create the Entry widget using tk.Entry
+    note_entry = tk.Entry(add_note, textvariable=note)
+    note_entry.pack()
+root = Window(themename="superhero")
 root.title("PyJottings")
-root.configure()
 root.geometry('300x660+40+40')
 
-settings_frame = tb.LabelFrame(root,
-                     text="Settings",
-                     width=80,
-                     height=640,
-                     )
-settings_frame.pack(pady=10,padx=10)
-settings_frame.place(x=10,y=10)
+style = Style('superhero')
 
-add_button = tb.Button(settings_frame,
-                           text="+")
-add_button.pack(pady=10,padx=10)
+settings_frame = LabelFrame(root,
+                            text="Settings",
+                            width=80,
+                            height=640)
+settings_frame.pack(pady=10, padx=10)
+settings_frame.place(x=10, y=10)
+
+add_button = Button(settings_frame,
+                    text="+",
+                    command=open_new_window)
+add_button.pack(pady=10, padx=10)
 
 root.mainloop()
