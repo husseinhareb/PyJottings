11e5c416b3b8727a1d2d48ef0f48836afd3840a5
husseinhareb <shtam.hareb12@gmail.com>
2023-11-14 17:35:39 +0200

    first

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Test");
+import tkinter as tk
+from tkinter import ttk
+from ttkbootstrap import Style
+
+root = tk.Tk()
+root.title("Simple Tkinter App")
+
+style = Style("lumen")
+
+mainframe = ttk.LabelFrame(root, text="Settings")
+mainframe.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)  # Adjust padx and pady as needed
+
+
+# Run the Tkinter event loop
+root.mainloop()
