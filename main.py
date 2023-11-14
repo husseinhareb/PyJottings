341ccd926090c3bb7e14cdb8a6fb1517c2f45cb9
husseinhareb <shtam.hareb12@gmail.com>
2023-11-14 18:04:01 +0200

    moved into ttkbootstrap

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from PIL import Image
 from ttkbootstrap.constants import *
 Image.CUBIC = Image.BICUBIC
 
-root = tk.Tk(className="monitoring_widget")
-theme = tb.Style("darkly")
-root.configure(bg="#3b3b3b")
+root = tb.Window(themename="superhero")
+root.title("PyJottings")
+root.configure()
 root.geometry('300x660')
 
-settings_frame = tb.Frame(root,
+settings_frame = tb.LabelFrame(root,
+                     text="Settings",
                      width=80,
-                     height=60,
+                     height=640,
                      )
 settings_frame.pack(pady=10,padx=10)
 settings_frame.place(x=10,y=10)
+
+
 root.mainloop()
