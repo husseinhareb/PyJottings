3b048212d6fb76da97aeb21f2a83ebfcee8378cb
husseinhareb <shtam.hareb12@gmail.com>
2023-11-14 20:04:03 +0200

    note handling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from ttkbootstrap import Style, Button, LabelFrame, Window
 
-def open_new_window():
+def add_note():
     add_note = tk.Toplevel(root)
     add_note.title("addnote")
     add_note.geometry('200x200')
@@ -9,7 +9,16 @@ def open_new_window():
 
     # Create the Entry widget using tk.Entry
     note_entry = tk.Entry(add_note, textvariable=note)
-    note_entry.pack()
+    note_entry.pack(padx = 10,pady=10)
+    note_accept = tk.Button(add_note,
+                    text="",
+                    )
+    note_accept.pack()
+    note_cancel = tk.Button(add_note,
+                   text="",
+                    )
+    note_cancel.pack()
+
 root = Window(themename="superhero")
 root.title("PyJottings")
 root.geometry('300x660+40+40')
@@ -25,7 +34,7 @@ settings_frame.place(x=10, y=10)
 
 add_button = Button(settings_frame,
                     text="+",
-                    command=open_new_window)
+                    command=add_note)
 add_button.pack(pady=10, padx=10)
 
 root.mainloop()
