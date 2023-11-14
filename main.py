6e6d069366dc9bfac9c84662910b862bf9006626
husseinhareb <shtam.hareb12@gmail.com>
2023-11-14 17:51:08 +0200

    added main frame

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import tkinter as tk
-from tkinter import ttk
-from ttkbootstrap import Style
+import ttkbootstrap as tb
+from PIL import Image
+from ttkbootstrap.constants import *
+Image.CUBIC = Image.BICUBIC
 
-root = tk.Tk()
-root.title("Simple Tkinter App")
+root = tk.Tk(className="monitoring_widget")
+theme = tb.Style("darkly")
+root.configure(bg="#3b3b3b")
+root.geometry('300x660')
 
-style = Style("lumen")
-
-mainframe = ttk.LabelFrame(root, text="Settings")
-mainframe.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)  # Adjust padx and pady as needed
-
-
-# Run the Tkinter event loop
+settings_frame = tb.Frame(root,
+                     width=80,
+                     height=60,
+                     )
+settings_frame.pack(pady=10,padx=10)
+settings_frame.place(x=10,y=10)
 root.mainloop()
